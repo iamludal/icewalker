78a993082074993d690f098081cddf7b33298830
Ludo <ludovic.chombeau@gmail.com>
2019-11-15 22:35:36 +0100

    Add Cell doctests + some methods
    Methods:
    - is_final_cell
    - set_final_cell
    - get_content
    - set_content

--- Cell.py
+++ Cell.py
@@ -2,20 +2,86 @@
 class Cell:
 
     def __init__(self):
+        """ Create a new cell for the icewalker grid
+
+        :Examples:
+        >>> c = Cell()
+        >>> c.get_walls()
+        set()
+        >>> c.is_final_cell()
+        False
+        >>> c.get_content() # Return nothing since it's None
+        """
         self.__content = None
         self.__walls = set()
+
+    def is_final_cell(self):
+        """ Return True if the cell if the final one, False otherwise
+
+        :return: (bool) True if the cell if the final one
+        :Examples:
+        >>> c = Cell()
+        >>> c.is_final_cell()
+        False
+        >>> c.set_final_cell()
+        >>> c.is_final_cell()
+        True
+        """
+        return self.__content == '☐'
+
+    def set_final_cell(self):
+        """ Define the current cell as the final one
+
+        :Examples:
+        >>> c = Cell()
+        >>> c.set_final_cell()
+        >>> c.is_final_cell()
+        True
+        """
+        self.__content = '☐'
 
     def is_empty(self):
         """ Return a boolean value that says whether or not the cell is empty
 
-        :return: True if the cell is empty, False otherwise
-        :rtype: bool
+        :return: (bool) True if the cell is empty, False otherwise
         :Examples:
         >>> c = Cell()
         >>> c.is_empty()
         True
+        >>> from Player import Player
+        >>> p = Player(1, 2, 0)
+        >>> c.set_content(p)
+        >>> c.is_empty()
+        False
         """
-        return self.__content is None
+        return self.__content == None
+
+    def get_content(self):
+        """ Get the content of the current cell
+
+        :Examples:
+        >>> c = Cell()
+        >>> c.get_content() # Returns nothing since it's 'None'
+        >>> c.set_final_cell()
+        >>> c.get_content()
+        '☐'
+        """
+        return self.__content
+
+    def set_content(self, content):
+        """ Define the new content of the cell
+
+        :param content: (any) the content to put in the cell
+        :UC: None
+        :Examples:
+        >>> c = Cell()
+        >>> from Player import Player
+        >>> p = Player(1, 2, 3)
+        >>> c.set_content(p)
+        >>> c.get_content()
+        Player 3
+        """
+        self.__content = content
 
     def add_wall(self, direction):
 
@@ -25,30 +91,70 @@ class Cell:
         self.__walls.add(direction)
 
     def get_walls(self):
+        """ Returns the walls of the cell
+
+        :return: (set) the walls around the cell
+        :Examples:
+        >>> c = Cell()
+        >>> c.get_walls()
+        set()
+        >>> c.add_wall('E')
+        >>> c.get_walls().issubset({'E'})
+        True
+        >>> c.add_wall('S')
+        >>> walls = c.get_walls()
+        >>> walls.issubset({'E', 'S'})
+        True
+        >>> 'E' in walls and 'S' in walls
+        True
+        """
         return self.__walls
 
     def __str__(self):
         """ Convert the cell into a string
 
-        :return: the string corresponding to the cell
-        :rtype: str
+        :return: (str) the string corresponding to the cell
         :Examples:
         >>> c = Cell()
         >>> str(c)
-        ' '
+        '  '
+        >>> c.set_final_cell()
+        >>> str(c)
+        '☐ '
+        >>> c.add_wall('E')
+        >>> str(c)
+        '☐|'
+        >>> from Player import Player
+        >>> p = Player(1, 2, 0)
+        >>> c.set_content(p)
+        >>> str(c)
+        '0|'
         """
 
-        if 'E' in self.__walls:
-            string = '|'
-        else:
-            string = ' '
-
         if self.is_empty():
-            string += ' '
+            string = ' '
         else:
-            string += str(self.__content)
+            string = str(self.__content)
+
+        if 'E' in self.__walls:
+            string += '|'
+        else:
+            string += ' '
 
         return string
+
+    def __repr__(self):
+        """ Get a representation of the cell
+
+        :Examples:
+        >>> c = Cell()
+        >>> c
+        <BLANKLINE>
+        >>> c.add_wall('E')
+        >>> c
+         |
+        """
+        return str(self)
 
 
 if __name__ == "__main__":
