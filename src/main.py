016b1f2b3b0b3a656924c1e43e325f376d5f8337
Ludo <ludovic.chombeau@gmail.com>
2019-11-19 08:09:38 +0100

    Add players images

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from Game import Game
 import pygame
 
 
-CELL_SIZE = 50
+CELL_SIZE = 60
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BORDER = 4
@@ -10,38 +10,54 @@ CYAN = (175, 240, 240)
 GRAY = (50, 180, 180)
 RADIUS = CELL_SIZE // 3
 
+LEO = pygame.image.load("../images/weinberg.png")
+NOE = pygame.image.load("../images/noe.png")
+EW = pygame.image.load("../images/ew.png")
+MEFT = pygame.image.load("../images/meftali.png")
+LEO = pygame.transform.scale(LEO, (CELL_SIZE, CELL_SIZE))
+NOE = pygame.transform.scale(NOE, (CELL_SIZE, CELL_SIZE))
+EW = pygame.transform.scale(EW, (CELL_SIZE, CELL_SIZE))
+MEFT = pygame.transform.scale(MEFT, (CELL_SIZE, CELL_SIZE))
+
+IMG = [EW, NOE, LEO, MEFT]
+
+
 def render(grid, win):
     width, height = grid.get_width(), grid.get_height()
 
     pygame.draw.rect(win, WHITE, (BORDER, BORDER, width*CELL_SIZE - BORDER*2,
-                                                height*CELL_SIZE - BORDER*2))
+                                  height*CELL_SIZE - BORDER*2))
 
     for x in range(width):
         for y in range(height):
-            
+
             walls = grid.get_cell(x, y).get_walls()
 
             if grid.get_cell(x, y).is_final_cell():
-                pygame.draw.rect(win, CYAN, (x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(win, CYAN, (x*CELL_SIZE, y *
+                                             CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
             if not grid.get_cell(x, y).is_empty():
-                pygame.draw.circle(win, GRAY, (x*CELL_SIZE + CELL_SIZE//2, y*CELL_SIZE + CELL_SIZE//2), RADIUS)
+                p = grid.get_cell(x, y).get_content()
+                win.blit(IMG[p.get_n()], (x*CELL_SIZE +
+                                          BORDER, y*CELL_SIZE + BORDER))
 
             if 'E' in walls:
-                pygame.draw.rect(win, BLACK, ((x+1)*CELL_SIZE - BORDER/2, y*CELL_SIZE, BORDER, CELL_SIZE))
+                pygame.draw.rect(
+                    win, BLACK, ((x+1)*CELL_SIZE - BORDER/2, y*CELL_SIZE, BORDER, CELL_SIZE))
 
             if 'S' in walls:
-                pygame.draw.rect(win, BLACK, (x*CELL_SIZE, (y+1)*CELL_SIZE - BORDER/2, CELL_SIZE, BORDER))
-
+                pygame.draw.rect(win, BLACK, (x*CELL_SIZE, (y+1)
+                                              * CELL_SIZE - BORDER/2, CELL_SIZE, BORDER))
 
             # x_bis = CELL_SIZE*y + MARGIN*y
             # y_bis = CELL_SIZE*x + MARGIN*x
             # pygame.draw.rect(win, , (x_bis, y_bis, CELL_SIZE, CELL_SIZE))
 
 
-
 if __name__ == "__main__":
-    import sys, json
+    import sys
+    import json
     sys.argv.pop(0)
     pygame.init()
 
@@ -59,10 +75,9 @@ if __name__ == "__main__":
 
     g = Game(filename)
     win = pygame.display.set_mode((width*CELL_SIZE, height*CELL_SIZE))
-    
 
     while True:
-        pygame.time.delay(1000)  # Refresh delay (1s)
+        pygame.time.delay(100)  # Refresh delay (1s)
         win.fill(BLACK)
         render(g.get_grid(), win)
         pygame.display.update()
